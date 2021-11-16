594d9a4cc7392d6e2301e2e7fca105cf1441c557
DevKS-Yet <ksang0324@gmail.com>
2021-11-17 01:23:15 +0900

    20211117

---
+++ momentum002/app.js
@@ -0,0 +1,16 @@
+var a = 1;
+const b = 2;
+
+let myName = "kyuSang";
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("hello " + myName);
+
+myName = "CKS";
+
+console.log("my new name is " + myName);
+
+// 초창기 자바스크립트에서는 var만 있었지만 변수와 상수를 구분 짓기 위해서 let과 const를 씀
+// 항상 const, 가끔 let, var은 노노!
